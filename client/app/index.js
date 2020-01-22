ae628bbd4734f59b03157d509d2037467fd6f789
matigumma <maalguna@gmail.com>
2020-01-21 21:23:22 -0300

    loadable

--- client/app/index.js
+++ client/app/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import LoadableApp from "./components/App/LoadableApp";
 import * as serviceWorker from './serviceWorker';
+
+import loadable from '@loadable/component'
+
+const LoadableApp = loadable(() => import('./components/App'))
 
 ReactDOM.render((<LoadableApp />), document.getElementById('root'));
 
 serviceWorker.unregister();
 
-if (module.hot && process.env.NODE_ENV === "development") {
+/* if (module.hot && process.env.NODE_ENV === "development") {
     module.hot.accept("./components/App/LoadableApp", () => {
       const NextApp = require("./components/App/LoadableApp").default;
       ReactDOM.render(<NextApp/>, rootId);
     });
-}
+} */
