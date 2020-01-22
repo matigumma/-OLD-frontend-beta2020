0f725d740718174cca0f7ee2e9a0f6cfd95fcd2f
matigumma <maalguna@gmail.com>
2020-01-22 00:00:29 -0300

    https://github.com/ReactTraining/react-router/issues/6420#issuecomment-433541079

--- client/app/components/App/App.jsx
+++ client/app/components/App/App.jsx
@@ -176,7 +176,7 @@ const App = () =>{
 						<Route exact path="/login" render={() => <LoginForm _login={_login} />}/>
 						<Route exact path="/user/:id" render={(props) => <UserProfile {...props} userState={user} />}/>
 						<Route exact path="/cam/:any" render={(state) => <CameraView {...state} cameras={cameras} userState={user} />}/>
-						<Route exact path="/signup" component={SignupForm} />
+						<Route exact path="/signup" component={() => <SignupForm />} />
 						<Route path="/404" render={(state) => <NotFound {...state}/>} />
 					</Switch>
 				</Suspense>
