12d4ae5a50663313a88359d6835482db04987331
matigumma <maalguna@gmail.com>
2020-01-24 17:40:42 -0300

    fix error app.jsx

--- client/app/index.js
+++ client/app/index.js
@@ -2,7 +2,7 @@ import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
 //import * as serviceWorker from './serviceWorker';
 import './styles/main.scss';
-const App = lazy(() =>import('.components/App/App.jsx'))
+const App = lazy(() =>import('./components/App/App.jsx'))
 //import App from './components/App/App.jsx'
 import Splash from './pages/Splash/Splash'
 
