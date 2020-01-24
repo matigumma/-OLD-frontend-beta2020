0f311151e9753ccefca7199ecc5bce92bd7f7d9e
matigumma <maalguna@gmail.com>
2020-01-23 22:08:58 -0300

    step-3

--- client/app/index.js
+++ client/app/index.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 import './styles/main.scss';
-import App from './components/App/App.jsx'
+import Loading from './components/Loading';
+//import App from './components/App/App.jsx'
+const App = lazy(() => import('./components/App/App.jsx'));
 
 /* import loadable from '@loadable/component'
 
 const LoadableApp = loadable(() => import('./components/App/App.jsx')) */
 
-ReactDOM.render((<App />), document.getElementById('root'));
+ReactDOM.render((<Suspense fallback={Loading}><App /></Suspense>), document.getElementById('root'));
 
-serviceWorker.unregister();
+//serviceWorker.unregister();
 
 /* if (module.hot && process.env.NODE_ENV === "development") {
     module.hot.accept("./components/App/LoadableApp", () => {
