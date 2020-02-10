4dc7117eee520b3d9e9e65e36bca2ed9b5e32581
matigumma <maalguna@gmail.com>
2020-02-09 23:45:48 -0300

    Update App.jsx

--- client/app/components/App/App.jsx
+++ client/app/components/App/App.jsx
@@ -179,7 +179,7 @@ const App = () =>{
 /* 	splash? <Loading />
 	: */
 	return (
-		<Router>
+		<Router onUpdate={() => window.scrollTo(0, 0)} history={createBrowserHistory()}>
 		<div className="h-100">
 			{/* <Header cameras={cameras} state={user} _logout={_logout} /> */}
 			<Header state={user} _logout={_logout} />
