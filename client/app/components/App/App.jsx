194190fe6e6c29ce384996f084b2a79ed9d57b39
matigumma <maalguna@gmail.com>
2020-02-12 21:30:41 -0300

    Update App.jsx

--- client/app/components/App/App.jsx
+++ client/app/components/App/App.jsx
@@ -186,8 +186,8 @@ const App = () =>{
 			<main className="h-100">
 				{/* <Route exact path="/" render={() => <Home ads={ads} cameras={cameras} userState={user} />} /> */}
 				<Route exact path="/" render={() => <Home userState={user} />} />
-				<Route exact path="/login" render={() => <LoginForm {...props} {...state}_login={_login} />}/>
-				<Route exact path="/user/:id" render={(props) => <UserProfile {...props} userState={user} />}/>
+				<Route exact path="/login" render={() => <LoginForm {...state}_login={_login} />}/>
+				<Route exact path="/user/:id" render={(props) => <UserProfile userState={user} />}/>
 				{/* <Route exact path="/cam/:any" render={(state) => <CameraView {...state} cameras={cameras} userState={user} />}/> */}
 				<Route exact path="/cam/:any" render={(state) => <CameraView {...state} userState={user} />}/>
 				<Route exact path="/signup" component={() => <SignupForm />} />
