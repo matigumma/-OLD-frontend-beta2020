6671c6696f08fe96a1ea075492cbfc392e94a38f
matigumma <maalguna@gmail.com>
2020-02-16 14:31:51 -0300

    Update App.jsx

--- client/app/components/App/App.jsx
+++ client/app/components/App/App.jsx
@@ -151,7 +151,7 @@ const App = () =>{
 					{/* <Route exact path="/cam/:any" render={(state) => <CameraView  cameras={cameras} userState={user} />}/> */}
 					<Route exact path="/cam/:any" render={(state) => <CameraView {...state} userState={user} />}/>
 					<Route exact path="/signup" component={() => <SignupForm />} />
-					<Route exact path="/validation/:id" component={() => <Validation {...state}/>} />
+					<Route exact path="/validation/:id" component={(state) => <Validation {...state}/>} />
 					<Route path="/404" render={(state) => <NotFound {...state}/>} />
 				</main>
 			</div>
